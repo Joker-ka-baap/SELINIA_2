a0d7e6f14c1f0a7b319ca67865e95e52c40a63a3
skap2020 <69468480+skap2020@users.noreply.github.com>
2020-09-27 11:13:00 +0530

    changed

---
+++ tg_bot/modules/abuse.py
@@ -0,0 +1,42 @@
+import random
+from telegram.ext import run_async, Filters
+from telegram import Message, Chat, Update, Bot, MessageEntity
+from tg_bot import dispatcher
+from tg_bot.modules.disable import DisableAbleCommandHandler
+
+SFW_STRINGS = (
+    "പോടാ പട്ടി",
+    "ഡാ പന്നി ",
+    "പുന്നാര മോനെ",
+    "പോടാ മാക്രി",
+    "പോടാ നാറി",
+    "വാടാ ... പട്ടീ",
+    "പോയി ചാവടാ",
+    "നീ പോടാ കാട്ടുകോഴി",
+    "പോയി ചത്തൂടെ നിനക്ക്",
+    "കോപ്പേ വല്യ ബഹളം വേണ്ട",
+    "വല്യ മലരനാണല്ലോടാ നീ",
+    "മണ്ണുണ്ണി",
+    "ഡാ പന്നക്കിളവ",
+    " നിന്റെ കുഞ്ഞമ്മേടെ നായർ",
+    "നിന്റെ അപ്പൂപ്പനോട്‌ പോയി പറ",
+    "പോ മലരേ"
+  )
+
+@run_async
+def dark(bot: Bot, update: Update):
+    bot.sendChatAction(update.effective_chat.id, "typing") # Bot typing before send messages
+    message = update.effective_message
+    if message.reply_to_message:
+      message.reply_to_message.reply_text(random.choice(SFW_STRINGS))
+    else:
+      message.reply_text(random.choice(SFW_STRINGS))
+
+
+
+
+__mod_name__ = "Abuse"
+
+DARK_HANDLER = DisableAbleCommandHandler("dark", dark)
+
+dispatcher.add_handler(DARK_HANDLER)
